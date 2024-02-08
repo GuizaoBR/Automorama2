2c1718aa245cc9e83fcee5bd2bcc83c5408d40a8
Guilherme Germano Silva <gui.germano.silva@gmail.com>
2024-02-08 20:13:47 -0300

    refac: removido imports desnecessários

--- composeApp/src/commonMain/kotlin/ui/Veiculos/VeiculosList.kt
+++ composeApp/src/commonMain/kotlin/ui/Veiculos/VeiculosList.kt
@@ -1,13 +1,9 @@
 package ui.Veiculos
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.FlowRow
@@ -16,11 +12,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
@@ -30,12 +24,8 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -129,7 +119,7 @@ fun VeiculosListScreen(
                             Spacer(modifier = Modifier.width(16.dp))
                             FlowColumn {
                                 Text(
-                                    text = "${veiculo.placa}",
+                                    text = veiculo.placa,
                                     style = TextStyle.Default.copy(
                                         fontWeight = FontWeight.Medium,
                                         fontSize = 20.sp
