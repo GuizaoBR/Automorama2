53d64c42a249c8a7a3c7e068240c436c00b772a1
= <=>
2024-03-02 01:58:24 -0300

    fix: Wrong code pushed on last commit

--- shared/src/nativeMain/kotlin/data/DriverFactory.native.kt
+++ shared/src/nativeMain/kotlin/data/DriverFactory.native.kt
@@ -3,22 +3,28 @@ package data
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import germano.guilherme.automorama2.Automorama2Database
-import helper.DatabaseUtils.doesDatabaseExist
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSSearchPathForDirectoriesInDomains
 import platform.Foundation.NSUserDomainMask
 import platform.Foundation.NSDocumentDirectory
 
 actual class DriverFactory {
-        actual fun createDriver(): SqlDriver {
-                val exist = doesDatabaseExist("Automorama2Database.db")
-                val nativeSqliteDriver =
-                        NativeSqliteDriver(Automorama2Database.Schema, "Automorama2Database.db")
-                if (!exist) {
-                        createCombustiveis(nativeSqliteDriver)
-                }
-                return nativeSqliteDriver
-        }
+       actual fun createDriver(): SqlDriver {
+        val paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true)
+        val documentsDirectory = paths[0] as String
+        val databasePath = "$documentsDirectory/Automorama2Database.db"
+
+        val exist = NSFileManager.defaultManager.fileExistsAtPath(databasePath)
+
+               println(databasePath)
+               println("$exist")
+               val nativeSqliteDriver =
+                       NativeSqliteDriver(Automorama2Database.Schema, "Automorama2Database.db")
+               if (!exist) {
+                       createCombustiveis(nativeSqliteDriver)
+               }
+               return nativeSqliteDriver
+       }
 }
 
 
