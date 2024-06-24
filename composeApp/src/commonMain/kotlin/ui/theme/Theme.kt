27be481ec23b07005dada1408309f3ae3b14951d
Guilherme Germano Silva <gui.germano.silva@gmail.com>
2024-06-24 18:16:16 -0300

    fix: alterado pacotes

--- composeApp/src/commonMain/kotlin/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/ui/theme/Theme.kt
@@ -1,11 +1,10 @@
-package com.example.compose
+package ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
-import com.example.ui.theme.AppTypography
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 
